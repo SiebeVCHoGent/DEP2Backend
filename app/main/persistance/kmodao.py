0b0a723e74e3aee9595e7b67de15ba11a7414e17
Siebe Van Campenhout <siebe.vancampenhout@student.hogent.be>
2022-11-30 16:11:45 +0100

    error handling

--- app/main/persistance/kmodao.py
+++ app/main/persistance/kmodao.py
@@ -1,18 +1,31 @@
+from app.main.common.exceptions import DBException
 from app.main.config import db
 
 
 def get_full_kmo(ondernemingsnummer: str):
-    kmo = dict(db.session.query(db.Kmo, db.Gemeente, db.Sector).join(db.Gemeente).join(db.Sector)\
-        .filter(db.Kmo.ondernemingsnummer == ondernemingsnummer).first())
-    kmo['verslagen'] = get_verslagen(ondernemingsnummer)
-    return kmo
+    try:
+        kmo = dict(db.session.query(db.Kmo, db.Gemeente, db.Sector).join(db.Gemeente).join(db.Sector)\
+            .filter(db.Kmo.ondernemingsnummer == ondernemingsnummer).first())
+        kmo['verslagen'] = get_verslagen(ondernemingsnummer)
+        return kmo
+    except Exception as e:
+        db.session.rollback()
+        raise DBException(f'Error while getting kmo from database {str(e)}')
 
 
 def get_verslagen(ondernemingsnummer: str):
-    verslagen = db.session.query(db.Verslag).filter(db.Verslag.ondernemingsnummer == ondernemingsnummer).all()
-    return verslagen
+    try:
+        verslagen = db.session.query(db.Verslag).filter(db.Verslag.ondernemingsnummer == ondernemingsnummer).all()
+        return verslagen
+    except Exception as e:
+        db.session.rollback()
+        raise DBException(f'Error while getting verslagen from database {str(e)}')
 
 
 def search_kmos(search: str):
-    kmos = db.session.query(db.Kmo).filter(db.Kmo.naam.match(search)).all()
-    return kmos
+    try:
+        kmos = db.session.query(db.Kmo).filter(db.Kmo.naam.match(search)).all()
+        return kmos
+    except Exception as e:
+        db.session.rollback()
+        raise DBException(f'Error while searching kmos from database {str(e)}')
